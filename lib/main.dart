d076eb44da8141e849091c18745ba0717aa73ccd
Lucas Schulz <LucasJohnschulz@gmail.com>
2026-06-30 18:08:56 -0400

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:csv/csv.dart';
-import 'package:path_provider/path_provider.dart';
-
 /*To find backup:
   first time on device
     go to files
